5deaaf6c9c524b71c50bf0dca6ddf686a0670a17
Shreyas Bapat <bapat.shreyas@gmail.com>
2018-01-12 03:36:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -14,21 +14,23 @@ place.date = datetime.now().date().strftime('%Y/%m/%d')
 
 test_place = place
 
-
+ra_req = eval(input("Enter the ra:  "))
+dec_req = eval(input("Enter the dec:  "))
 m45 = ephem.FixedBody()
-m45.name = "M 45"
-m45._ra = '3:47:24.0'
-m45._dec = '24:7:0.0'
-
-jupiter = ephem.Jupiter()
-jupiter.compute(test_place)
+m45.name = "M 45"Choose_Item = eval(input("Select your item:  "))
+#m45._ra = '3:47:24.0'
+#m45._dec = '24:7:0.0'
+m45._ra = ra_req
+m45._dec = dec_req
+# jupiter = ephem.Jupiter()
+# jupiter.compute(test_place)
 
 test_place.date = ephem.now()
 m45.compute(test_place)
-print (m45.name)
+# print (m45.name)
 print (degrees(m45.alt)) 
 print (degrees(m45.az)) 
 
-print (jupiter.name)
-print (degrees(jupiter.alt)) 
-print (degrees(jupiter.az)) 
+# print (jupiter.name)
+# print (degrees(jupiter.alt)) 
+# print (degrees(jupiter.az)) 
