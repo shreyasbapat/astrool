8459aff35d717a4107367e7cb7bf72c77b8c93d5
illright <leva181777@gmail.com>
2018-10-13 23:57:54 +0300

    Tests for SkyStars added

--- src/astrool/tests/test_radec.py
+++ src/astrool/tests/test_radec.py
@@ -1,8 +1,22 @@
+import ephem
 import pytest
 
 from astrool.radec import SolarMassive, SkyStars, SkyCustom
 
+
 def test_SolarMassive_pops_Value_Error():
     body = "Random"
     with pytest.raises(ValueError):
-        a = SolarMassive(body).get()
+        SolarMassive(body).get()
+
+
+def test_SkyStars_creation():
+    body = 'Polaris'
+    assert isinstance(SkyStars(body).get(), ephem.FixedBody)
+
+    bad_body = 'Random'
+    with pytest.raises(ValueError):
+        SkyStars(bad_body).get()
+
+    body_badcase = 'pOlArIs'
+    assert isinstance(SkyStars(body_badcase).get(), ephem.FixedBody)
